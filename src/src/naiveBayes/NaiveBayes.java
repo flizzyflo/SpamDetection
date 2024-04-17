440c54a6454b79aa2ff97f4d9e95672ddd1c29bd
flizzyflo <vip3r1@gmx.de>
2024-04-17 19:48:16 +0200

    added function signature naive bayes

--- src/src/naiveBayes/NaiveBayes.java
+++ src/src/naiveBayes/NaiveBayes.java
@@ -1,7 +1,20 @@
 package naiveBayes;
 
+import java.util.HashMap;
+
 public class NaiveBayes {
 
+    public HashMap<String, Double> calculateClassProbability() {
 
+        return null;
+    };
+
+    public void train() {
+
+    }
+
+    public String predict() {
+        return null;
+    }
 
 }
