f16ed44330e76cb7251e7fc71569d270e111597b
flizzyflo <vip3r1@gmx.de>
2024-04-19 21:19:19 +0200

    refactor variable names

--- src/src/naiveBayes/NaiveBayes.java
+++ src/src/naiveBayes/NaiveBayes.java
@@ -2,61 +2,58 @@ package naiveBayes;
 
 import wordCounter.WordCounter;
 
+//TODO klassen flexibler ermöglichen
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class NaiveBayes {
 
-    private final List<String> differentClasses;
-    private final HashMap<String, HashMap<String, Double>> wordProbabilitiesPerClass;
-    private final HashMap<String, Double> singleClassProbabilites;
-
+    private final List<String> differentClassNames;
+    private final HashMap<String, HashMap<String, Double>> wordProbabilitiesPerEverySingleClass;
+    private final HashMap<String, Double> classProbability;
+    private final HashMap<String, Double> probabilityEstimationPerClass;
 
     public NaiveBayes(HashMap<String, Double> singleClassProbabilites) {
-        this.differentClasses = new ArrayList<String>();
-        this.wordProbabilitiesPerClass = new HashMap<>();
-        this.singleClassProbabilites = singleClassProbabilites;
+        this.differentClassNames = new ArrayList<String>();
+        this.wordProbabilitiesPerEverySingleClass = new HashMap<>();
+        this.classProbability = singleClassProbabilites;
+        this.probabilityEstimationPerClass = new HashMap<String, Double>();
     }
 
-    public void addDifferentClasses(List<String> classNames) {
-        for (String className: classNames) {
-            this.addSingleClass(className);
-        }
-    }
-
-    private void countClassOccurence() {
-
+    public HashMap<String, Double> getProbabilityEstimationPerClass() {
+        return this.probabilityEstimationPerClass;
     }
 
     public void addSingleClass(String className) {
-        this.differentClasses.add(className);
+        this.differentClassNames.add(className);
     }
 
     public void calculateProbabilityPerClass(String className, WordCounter wordCounterForClass) {
-        this.wordProbabilitiesPerClass.put(className, wordCounterForClass.getWordProbabilities());
+        this.wordProbabilitiesPerEverySingleClass.put(className, wordCounterForClass.getWordProbabilities());
 
     };
 
     public String predict(WordCounter wordCounterForTextToBePredicted) {
         HashMap<String, Integer> wordCount = wordCounterForTextToBePredicted.getWordCount();
 
-        double spamProbability = this.calculateProbabiltyForAllWords(this.wordProbabilitiesPerClass, "spam", wordCount);
-        double nonSpamProbabilty = this.calculateProbabiltyForAllWords(this.wordProbabilitiesPerClass, "kein_spam", wordCount);
+        double spamProbability = this.calculateProbabiltyForAllWords(this.wordProbabilitiesPerEverySingleClass, "spam", wordCount);
+        double nonSpamProbabilty = this.calculateProbabiltyForAllWords(this.wordProbabilitiesPerEverySingleClass, "kein_spam", wordCount);
 
         return spamProbability > nonSpamProbabilty ? "Spam" : "kein Spam";
 
     };
 
-    private double calculateProbabiltyForAllWords(HashMap<String, HashMap<String, Double>> classProbabilities, String className, HashMap<String, Integer> wordCount) {
+    private double calculateProbabiltyForAllWords(HashMap<String, HashMap<String, Double>> wordProbabilityPerClass, String className, HashMap<String, Integer> wordCount) {
 
-        double probability = this.singleClassProbabilites.get(className);
+        double probability = this.classProbability.get(className);
 
-        HashMap<String, Double> classProbability = classProbabilities.get(className);
+        HashMap<String, Double> wordProbabilities = wordProbabilityPerClass.get(className);
 
         for (String word: wordCount.keySet()) {
             int count = wordCount.get(word);
-            double wordProbability = classProbability.getOrDefault(word, 0.0); // Probability of the word in the class
+            double wordProbability = wordProbabilities.get(word); // Probability of the word in the class
             probability *= Math.pow(wordProbability, count); // Multiply the probability for each word
         }
 
