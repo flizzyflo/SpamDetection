b74251edd30b7117c3307e374aca1a74a3bd4c4f
flizzyflo <vip3r1@gmx.de>
2024-04-19 22:55:06 +0200

    implemented Punctuation.java class having all punctuations in itself

--- src/src/wordCounter/Punctuation.java
+++ src/src/wordCounter/Punctuation.java
@@ -1,4 +1,12 @@
 package wordCounter;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Punctuation {
+
+    public static List<String> PUNCTUATION = Arrays.asList(new String[]{
+            "!", "#", "$", "%", "&", "\"", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@",
+            "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~", String.valueOf('"')});
+
 }
