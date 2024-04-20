9bd40779ebe1a1ae0b43ced4fc7911955a6b6958
flizzyflo <vip3r1@gmx.de>
2024-04-20 11:22:23 +0200

    deleted unused vars

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,6 +1,7 @@
 import naiveBayes.NaiveBayes;
 import wordCounter.WordCounter;
 
+import java.util.HashMap;
 import java.util.List;
 
 
@@ -11,15 +12,14 @@ public class Main {
         final String spam = "spam";
 
         WordCounter nonSpamCounter = new WordCounter("/Users/florianluebke/Desktop/SpamDetection/SpamDetection/src/learningData/notSpam", ".txt", notSpam, true);
-        WordCounter spamCounter = new WordCounter("/Users/florianluebke/Desktop/SpamDetection/SpamDetection/src/learningData/spam", ".txt", spam, true);
+        WordCounter spamCounter = new WordCounter("/Users/florianluebke/Desktop/SpamDetection/SpamDetection/src/learningData/spam", ".txt",   spam, true);
         WordCounter testCounter = new WordCounter("/Users/florianluebke/Desktop/SpamDetection/SpamDetection/src/learningData/testfile", ".txt", true);
 
-        List<WordCounter> w = WordCounter.getWordCounters();
-        NaiveBayes nb = new NaiveBayes();
+        HashMap<String, HashMap<String, Integer>> w = WordCounter.getWordCountPerClass();
+
+        NaiveBayes nb = new NaiveBayes(1, WordCounter.getClassificationAppearences());
         nb.train(w);
+        System.out.println(nb.predict(testCounter));
 
-        String resClass = nb.predict(testCounter);
-        System.out.println(resClass);
-
-        }
     }
+}
