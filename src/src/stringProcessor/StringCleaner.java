4a2b5f18d5cf6f2fdb90400e19bd095e4c9af486
flizzyflo <vip3r1@gmx.de>
2024-04-25 23:19:38 +0200

    implemented string cleaner class to apply different cleaning strategies

---
+++ src/src/stringProcessor/StringCleaner.java
@@ -0,0 +1,10 @@
+package stringProcessor;
+
+public class StringCleaner {
+
+    public String apply(StringProcessor stringProcessor, String s) {
+
+        return stringProcessor.doProcessing(s);
+    }
+
+}
