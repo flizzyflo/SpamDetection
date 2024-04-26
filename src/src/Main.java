98212d2652d4ebb8b69065bc2c404d419e3c7ba8
flizzyflo <vip3r1@gmx.de>
2024-04-26 10:15:45 +0200

    refactor

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,30 +1,13 @@
 import naiveBayes.NaiveBayes;
 import wordCounter.WordCounter;
-
 import java.util.HashMap;
 
 
 public class Main {
     public static void main(String[] args) {
 
-/*
-        final String notSpam = "notSpam";
-        final String spam = "spam";
-        final String uncl = "unclear";
-
-        WordCounter nonSpamCounter = new WordCounter("/Users/florianluebke/Desktop/SpamDetection/SpamDetection/src/learningData/notSpam", ".txt", notSpam, true);
-        WordCounter spamCounter = new WordCounter("/Users/florianluebke/Desktop/SpamDetection/SpamDetection/src/learningData/spam", ".txt",   spam, true);
-        WordCounter unclear = new WordCounter("/Users/florianluebke/Desktop/SpamDetection/SpamDetection/src/learningData/unclear", ".txt",   uncl, true);
-        WordCounter testCounter = new WordCounter("/Users/florianluebke/Desktop/SpamDetection/SpamDetection/src/learningData/testfile", ".txt", true);
-
-*/
-        WordCounter automobile = new WordCounter("/Users/florianluebke/Desktop/SpamDetection/SpamDetection/src/learningData/automobile", ".txt", "automobile", true);
-        WordCounter entertainment = new WordCounter("/Users/florianluebke/Desktop/SpamDetection/SpamDetection/src/learningData/entertainment", ".txt", "entertainment", true);
-        WordCounter politics = new WordCounter("/Users/florianluebke/Desktop/SpamDetection/SpamDetection/src/learningData/politics", ".txt", "politics", true);
-        WordCounter science = new WordCounter("/Users/florianluebke/Desktop/SpamDetection/SpamDetection/src/learningData/science", ".txt", "science", true);
-        WordCounter sports = new WordCounter("/Users/florianluebke/Desktop/SpamDetection/SpamDetection/src/learningData/sports", ".txt", "sports", true);
-        WordCounter technology = new WordCounter("/Users/florianluebke/Desktop/SpamDetection/SpamDetection/src/learningData/technology", ".txt", "technology", true);
-        WordCounter world = new WordCounter("/Users/florianluebke/Desktop/SpamDetection/SpamDetection/src/learningData/world", ".txt", "world", true);
+        WordCounter spam = new WordCounter("/Users/florianluebke/Desktop/SpamDetection/SpamDetection/src/learningData/spam", ".txt", "spam", true);
+        WordCounter notSpam = new WordCounter("/Users/florianluebke/Desktop/SpamDetection/SpamDetection/src/learningData/notSpam", ".txt", "not spam", true);
 
         WordCounter testCounter = new WordCounter("/Users/florianluebke/Desktop/SpamDetection/SpamDetection/src/learningData/testfile", ".txt", true);
 
